06473982303a297cb916444f6e43c9c2da0d938d
Pone <jacopo.lancione@gmail.com>
2024-02-16 10:48:17 +0100

    Notes and minor changes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,4 +1,4 @@
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true } -- you basically want to set these up (maybe one day try to comment them out and see what happens)
 
 local term_opts = { silent = true }
 
@@ -6,7 +6,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>", opts) -- in sostanza per farlo stai prima rimappando Space su No op e poi gli dici che la leader key e' " "
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -19,13 +19,14 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
+-- Better window navigation (qua stai in sostanza rimappando dei keymaps gia' esistenti)
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+-- qua invece stai proprio creando 1 nuovo keybinding, ecco che allora al posto del vecchio keybinding ci devi mettere il comando che vuoi che sia eseguito
+keymap("n", "<leader>e", ":Lex 30<cr>", opts) -- stands for left hand explorer and 30 is the width
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
@@ -33,13 +34,13 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- Navigate buffers (they are something like a history of what you have opend in the past on that window, really useful!)
+keymap("n", "<S-l>", ":bnext<CR>", opts) -- S stands for shift
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+-- Press jk fast to enter normal mode
+keymap("i", "hl", "<ESC>", opts) -- I believe hl is better than jk with the dvorak
 
 -- Visual --
 -- Stay in indent mode
@@ -47,12 +48,12 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts) -- A stands for the alt key
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
--- Move text up and down
+-- Move text up and down (qsto in sostanza fa la stsa cosa di qlo subito qua sopra e credo sia in realta' lo scopo dla visual block mode
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
